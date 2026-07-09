8d911990510b5b77077fa02f55ba1b7e77767f2b
maisonatelier52-ux <maisonatelier52-ux@github.com>
2026-07-09 17:33:04 +0530

    change5

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -6,7 +6,7 @@ export const getDatabaseConfig = (
 ): MongooseModuleOptions => {
   const isProd = process.env.NODE_ENV === 'production';
   const mongoUri = isProd
-    ? 'mongodb+srv://oakshun:oakshun123@cluster0.kvlibgx.mongodb.net/oakshun_db?retryWrites=true&w=majority&appName=Cluster0'
+    ? 'mongodb+srv://oakshun:oakshun123@cluster0.kvlibgx.mongodb.net/?appName=Cluster0'
     : 'mongodb://localhost:27017/oakshun_db';
 
   return {
