d2699b25616a7a2382157c10a5f5af1d2eb6a374
maisonatelier52-ux <maisonatelier52-ux@github.com>
2026-07-09 17:55:07 +0530

    change7

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,7 +5,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
-import { UsersModule } from 'src/users/users.module';
+import { UsersModule } from '../users/users.module';
 import { MailService } from './mail.service';
 import { OtpService } from './otp.service';
 
